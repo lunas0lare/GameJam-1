2e4ab666dc4c145c7f36bab1cfa21ddeee41cdfa
lunas0lare <heartbreaker2605@gmail.com>
2025-07-19 13:39:38 -0700

    ctreaing score and displaying, adjusting the random spawn of the mobs, removed dash for main character

--- test.py
+++ test.py
@@ -35,20 +35,32 @@ class mob(unit):
        super().__init__(location, rect)
     
     def move(self, Movement: int):
-        self.location.y += Movement
-        
+        self.location.x = Movement.x
+        self.location.y = Movement.y
+
+def update_Score():
+    global score
+    score += 1   
 
 class handling_things():
     def __init__(self):
         pass
-    def handle_collision(self, me: unit, mob: unit):
+    def handle_collision(self, me: unit, mob: unit) -> Vector2:
         if(me.rect.colliderect(mob.rect) == True):
-            mob.move(randint(-100, 100))
-
-
+            update_Score()
+           
+            return Vector2(randint(0, 700), randint(0, 500))
+            
+        return mob.location
+    
 class Game():
     
     def __init__(self):
+        pygame.init()
+        #text font
+        self.text_font = pygame.font.Font('Pixeltype.ttf', 50)
+        #creating score
+        self.score = 0
         #frame per second
         self.FPS = 10
         #movement speed of object
@@ -56,17 +68,24 @@ class Game():
         self.modMovementSpeed = 2
 
         global windowPos 
-        windowPos = Vector2(1080, 800)
-        pygame.init()
+        global score
+        score = 0
+
+        windowPos = Vector2(736, 523)
+       
         
         self.handle_object = handling_things()
 
         self.window = pygame.display.set_mode(windowPos)
 
-        #loading object
-        self.main = pygame.image.load('trash assets/box 1.png').convert_alpha()
-        self.mob = pygame.image.load('trash assets/recycling 1.png').convert_alpha()
+        #loading background 
+        self.backgound_1 = pygame.image.load('background/109th update_ Metropolis 28_?.jpg').convert_alpha()
 
+        #loading object
+        self.main = pygame.image.load('trash assets/recycling 2.png').convert_alpha()
+        self.mob = pygame.image.load('trash assets/box tile 32X32 4.png').convert_alpha()
+
+       
         #displaying game
         pygame.display.set_caption("TrashCanDoIt")
         self.clock = pygame.time.Clock()
@@ -75,12 +94,14 @@ class Game():
         self.mobsMovement = Vector2(0,0)
         self.meMovement = Vector2(0,0)
 
+        #render background
+        self.window.blit(self.backgound_1, (0,0))
         #creating rectangle for collision and rendering
         
-        self.meLocation = pygame.math.Vector2(500, 500)
+        self.meLocation = pygame.math.Vector2(500, 300)
         self.me_rect = self.window.blit(self.main, self.meLocation)#location of me object x = y = 500
-
-        self.mobLocation = pygame.math.Vector2(700, 400)
+       
+        self.mobLocation = pygame.math.Vector2(500, 400)
         self.mob_rect = self.window.blit(self.mob, self.mobLocation)
         
         self.unit = [
@@ -91,7 +112,6 @@ class Game():
     
     def processInput(self):
         self.meMovement = Vector2(0,0)
-        
         for event in pygame.event.get():
             pygame.key.set_repeat(1, self.FPS)
             if event.type == pygame.QUIT:
@@ -111,15 +131,25 @@ class Game():
                     self.meMovement.y = self.meMovementSpeed
                 elif event.key == pygame.K_UP:
                     self.meMovement.y = -self.meMovementSpeed
+                
                     
     def update(self):
         self.unit[0].move(self.meMovement)#when convert into vector, only need to truyen vao class
-        # self.mobsMovement.y = 1
-        self.handle_object.handle_collision(self.unit[0], self.unit[1])
-
+        
+        mobsMovement = self.handle_object.handle_collision(self.unit[0], self.unit[1])
+        self.unit[1].move(mobsMovement)
+       
+        
     def render(self):
-        self.window.fill((0,0,0))
 
+        #fill window color and background
+        self.window.fill((0,0,0))
+        self.window.blit(self.backgound_1, (0,0))
+        
+        #render and displaying score
+        self.text_surface = self.text_font.render(f'{score}', False, 'white')
+        self.window.blit(self.text_surface, (5, 5))
+        
         self.me_rect = self.window.blit(self.main, (self.unit[0].location.x, self.unit[0].location.y))
         self.unit[0].rect = self.me_rect
         self.mob_rect = self.window.blit(self.mob, (self.unit[1].location.x, self.unit[1].location.y))
