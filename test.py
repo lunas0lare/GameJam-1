30351e192dda8b360ec456bb4b5db17787fd109e
lunas0lare <heartbreaker2605@gmail.com>
2025-07-20 00:18:23 -0700

    add 2 more mobs, add game end screen

--- test.py
+++ test.py
@@ -44,15 +44,16 @@ def update_Score():
 
 class handling_things():
     def __init__(self):
-        pass
+        self.sound = pygame.mixer.Sound('retro-coin-1-236677.mp3')
     def handle_collision(self, me: unit, mob: unit) -> Vector2:
         if(me.rect.colliderect(mob.rect) == True):
             update_Score()
-           
+            self.sound.play()
             return Vector2(randint(0, 700), randint(0, 500))
             
         return mob.location
-    
+
+
 class Game():
     
     def __init__(self):
@@ -67,6 +68,11 @@ class Game():
         self.meMovementSpeed = 6
         self.modMovementSpeed = 2
 
+        #creating countdown
+        self.countdown_value = 31
+        
+        #random mobs's location
+        
         global windowPos 
         global score
         score = 0
@@ -78,20 +84,25 @@ class Game():
 
         self.window = pygame.display.set_mode(windowPos)
 
+        #loading game over screen
+        self.gameover_screen = pygame.image.load('game_over.jpg').convert_alpha()
         #loading background 
         self.backgound_1 = pygame.image.load('background/109th update_ Metropolis 28_?.jpg').convert_alpha()
 
         #loading object
         self.main = pygame.image.load('trash assets/recycling 2.png').convert_alpha()
-        self.mob = pygame.image.load('trash assets/box tile 32X32 4.png').convert_alpha()
 
+        self.mob1 = pygame.image.load('trash assets/box tile 32X32 4.png').convert_alpha()
+        self.mob2 = pygame.image.load('trash assets/box tile 32X32 3.png').convert_alpha()
+        self.mob3 = pygame.image.load('trash assets/box tile 32X32 2.png').convert_alpha()
        
+        self.mob_list = [self.mob1, self.mob2, self.mob3]
         #displaying game
         pygame.display.set_caption("TrashCanDoIt")
         self.clock = pygame.time.Clock()
 
         #creating movement variables
-        self.mobsMovement = Vector2(0,0)
+        # self.mobsMovement = Vector2(0,0)
         self.meMovement = Vector2(0,0)
 
         #render background
@@ -101,13 +112,19 @@ class Game():
         self.meLocation = pygame.math.Vector2(500, 300)
         self.me_rect = self.window.blit(self.main, self.meLocation)#location of me object x = y = 500
        
-        self.mobLocation = pygame.math.Vector2(500, 400)
-        self.mob_rect = self.window.blit(self.mob, self.mobLocation)
-        
+        self.mobLocation_1 = pygame.math.Vector2(randint(0, 700), randint(0, 500))
+        self.mobLocation_2 = pygame.math.Vector2(randint(0, 700), randint(0, 500))
+        self.mobLocation_3 = pygame.math.Vector2(randint(0, 700), randint(0, 500))
+        self.mob_rect_1 = self.window.blit(self.mob1, self.mobLocation_1)
+        self.mob_rect_2 = self.window.blit(self.mob2, self.mobLocation_2)
+        self.mob_rect_3 = self.window.blit(self.mob3, self.mobLocation_3)
+
         self.unit = [
-            me(self.meLocation, self.me_rect),
-            mob(self.mobLocation, self.mob_rect)
+            mob(self.mobLocation_1, self.mob_rect_1),
+            mob(self.mobLocation_2, self.mob_rect_2),
+            mob(self.mobLocation_3, self.mob_rect_3)
                      ]
+        self.me = me(self.meLocation, self.me_rect)
         self.running = True
     
     def processInput(self):
@@ -134,30 +151,49 @@ class Game():
                 
                     
     def update(self):
-        self.unit[0].move(self.meMovement)#when convert into vector, only need to truyen vao class
+        self.me.move(self.meMovement)#when convert into vector, only need to truyen vao class
         
-        mobsMovement = self.handle_object.handle_collision(self.unit[0], self.unit[1])
-        self.unit[1].move(mobsMovement)
+        for mob in self.unit:
+            mobsMovement = self.handle_object.handle_collision(self.me, mob)
+            mob.move(mobsMovement)
        
         
     def render(self):
-
         #fill window color and background
-        self.window.fill((0,0,0))
-        self.window.blit(self.backgound_1, (0,0))
-        
-        #render and displaying score
-        self.text_surface = self.text_font.render(f'{score}', False, 'white')
-        self.window.blit(self.text_surface, (5, 5))
-        
-        self.me_rect = self.window.blit(self.main, (self.unit[0].location.x, self.unit[0].location.y))
-        self.unit[0].rect = self.me_rect
-        self.mob_rect = self.window.blit(self.mob, (self.unit[1].location.x, self.unit[1].location.y))
-        self.unit[1].rect = self.mob_rect
+        self.finish = True
        
+        if(self.finish != False):
+            self.window.fill((0,0,0))
+            self.window.blit(self.backgound_1, (0,0))
+            
+            #render countdown
+            self.countdown = int(self.countdown_value - pygame.time.get_ticks() / 1000)
+            self.countdown_surface = self.text_font.render(f'{self.countdown}', False, 'yellow')
+            self.window.blit(self.countdown_surface, (700, 5))
+            
+            #render and displaying score
+            self.text_surface = self.text_font.render(f'{score}pt', False, 'white')
+            self.window.blit(self.text_surface, (5, 5))
+            
+            self.me_rect = self.window.blit(self.main, (self.me.location.x, self.me.location.y))
+            self.me.rect = self.me_rect
+
+            temp = 0
+            for mob in self.mob_list:
+                self.mob_rect = self.window.blit(mob, (self.unit[temp].location.x, self.unit[temp].location.y))
+                self.unit[temp].rect = self.mob_rect
+                temp = temp + 1
+            if(self.countdown <= 0):
+                self.finish = False
+        if(self.finish == False):
+            self.window.blit(self.gameover_screen, (-130, -230)) 
+            self.window.blit(self.text_font.render('your score: ' + f'{score}', False, 'black'), (230, 450))
+           
+           
         pygame.display.update()
         
     def run(self):
+        
         while self.running:
             self.processInput()
             self.update()
